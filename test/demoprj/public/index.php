c5ecb948582c47b262fe7ca89b4b4953920f3145
sasumi <cnsasumi@gmail.com>
2023-05-31 19:49:21 +0800

    upd

--- test/demoprj/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use function LFPhp\Plite\start_web;
-
-define('APP_ROOT', basename(__DIR__));
-define('SITE_ROOT', 'http://localhost/PLite/test/demoprj/public/index.php');
-include "../../../vendor/autoload.php";
-
-start_web();
