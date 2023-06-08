f44604d099f482f475bdad7bb2a3b634a08521bf
sasumi <cnsasumi@gmail.com>
2023-06-09 00:05:15 +0800

    upd

--- src/Exception/MessageException.php
+++ src/Exception/MessageException.php
@@ -8,18 +8,22 @@ use Throwable;
 class MessageException extends PLiteException implements JsonSerializable {
 	public $data;
 	public $forward_url;
+	public static $CODE_SUCCESS = 0;
+	public static $CODE_ERROR = -1;
 
 	public function __construct($message = "", $code = null, $data = null, $forward_url = '', Throwable $previous = null){
+		$code = $code ?? self::$CODE_ERROR;
 		parent::__construct($message, $code, $previous);
 		$this->data = $data;
 		$this->forward_url = $forward_url;
 	}
 
 	public static function successData($data, $message = 'success'){
-		return new self($message, null, $data);
+		return new self($message, self::$CODE_SUCCESS, $data);
 	}
 
 	public static function errorMessage($message, $code = null, $data = null){
+		$code = $code ?? self::$CODE_ERROR;
 		return new self($message, $code, $data);
 	}
 
