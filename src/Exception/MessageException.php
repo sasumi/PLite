3e8b104258af27c4ded7125f4c6ca3ea4dab4b03
sasumi <cnsasumi@gmail.com>
2023-06-09 17:56:56 +0800

    upd

--- src/Exception/MessageException.php
+++ src/Exception/MessageException.php
@@ -8,22 +8,42 @@ use Throwable;
 class MessageException extends PLiteException implements JsonSerializable {
 	public $data;
 	public $forward_url;
-	public static $CODE_SUCCESS = 0;
-	public static $CODE_ERROR = -1;
+	public static $CODE_DEFAULT_SUCCESS = 0; //默认成功码
+	public static $CODE_DEFAULT_ERROR = -1; //默认失败码
 
+	/**
+	 * @param string $message
+	 * @param null $code
+	 * @param null $data
+	 * @param string $forward_url
+	 * @param \Throwable|null $previous
+	 */
 	public function __construct($message = "", $code = null, $data = null, $forward_url = '', Throwable $previous = null){
-		$code = $code ?? self::$CODE_ERROR;
+		$code = $code ?? self::$CODE_DEFAULT_ERROR;
 		parent::__construct($message, $code, $previous);
 		$this->data = $data;
 		$this->forward_url = $forward_url;
 	}
 
-	public static function successData($data, $message = 'success'){
-		return new self($message, self::$CODE_SUCCESS, $data);
+	/**
+	 * 成功message
+	 * @param $data
+	 * @param string $message
+	 * @param string $forward_url
+	 * @return \LFPhp\PLite\Exception\MessageException
+	 */
+	public static function successData($data, $message = 'success', $forward_url = ''){
+		return new self($message, self::$CODE_DEFAULT_SUCCESS, $data, $forward_url);
 	}
 
+	/**
+	 * @param $message
+	 * @param null $code
+	 * @param null $data
+	 * @return \LFPhp\PLite\Exception\MessageException
+	 */
 	public static function errorMessage($message, $code = null, $data = null){
-		$code = $code ?? self::$CODE_ERROR;
+		$code = $code ?? self::$CODE_DEFAULT_ERROR;
 		return new self($message, $code, $data);
 	}
 
@@ -55,7 +75,7 @@ class MessageException extends PLiteException implements JsonSerializable {
 	}
 
 	/**
-	 * @return mixed
+	 * @return string
 	 */
 	public function getForwardUrl(){
 		return $this->forward_url;
