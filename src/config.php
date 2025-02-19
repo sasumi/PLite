96774d008c0f287cde65dc2b8153f0885f45526b
sasumi <cnsasumi@gmail.com>
2025-02-19 19:23:39 +0800

    upd

--- src/config.php
+++ src/config.php
@@ -8,12 +8,13 @@ use function LFPhp\Func\array_get;
  * The configuration file path is specified by PLITE_CONFIG_PATH, the configuration file format is file.inc.php, and the file returns an associative array
  * @param string $config_key_uri configuration name/path
  * @param bool $ignore_on_file_no_exists whether to ignore the situation where the file does not exist (the default is to force the file to exist)
+ * @param bool $nocache
  * @return array|mixed
  * @throws \Exception
  */
-function get_config($config_key_uri, $ignore_on_file_no_exists = false){
+function get_config($config_key_uri, $ignore_on_file_no_exists = false, $nocache = false){
 	static $cache = [];
-	if(isset($cache[$config_key_uri])){
+	if(!$nocache && isset($cache[$config_key_uri])){
 		return $cache[$config_key_uri];
 	}
 	$path = explode('/', $config_key_uri);
