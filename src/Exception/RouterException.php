c46c4e63ebf2aaa2582712efed1d33a298642638
Swing <cnsasumi@gmail.com>
2025-10-18 23:13:20 +0800

    upd

--- src/Exception/RouterException.php
+++ src/Exception/RouterException.php
@@ -6,7 +6,7 @@ use function LFPhp\Func\get_client_ip;
 use function LFPhp\Func\http_get_current_page_url;
 
 class RouterException extends PLiteException {
-	public function __construct($message = "", $code = null, Throwable $previous = null){
+	public function __construct($message = "", $code = 0, Throwable $previous = null){
 		parent::__construct($message, $code, $previous);
 		$this->data = [
 			'url'        => http_get_current_page_url(),
