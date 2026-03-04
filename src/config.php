b9d137b355dfb159b615e2cf1fb4f9b5fcca4ada
Swing <cnsasumi@gmail.com>
2026-03-04 23:03:19 +0800

    upd

--- src/config.php
+++ src/config.php
@@ -3,6 +3,11 @@ namespace LFPhp\PLite;
 
 use LFPhp\PLite\Exception\PLiteException as Exception;
 use function LFPhp\Func\array_get;
+use function LFPhp\Func\array_push_by_path;
+
+// Shared cache for config values
+$GLOBALS['_PLITE_CONFIG_CACHE'] = [];
+
 /**
  * Get configuration value of configuration file
  * The configuration file path is specified by PLITE_CONFIG_PATH, the configuration file format is file.inc.php, and the file returns an associative array
@@ -13,9 +18,8 @@ use function LFPhp\Func\array_get;
  * @throws \Exception
  */
 function get_config($config_key_uri, $ignore_on_file_no_exists = false, $nocache = false){
-	static $cache = [];
-	if(!$nocache && isset($cache[$config_key_uri])){
-		return $cache[$config_key_uri];
+	if(!$nocache && isset($GLOBALS['_PLITE_CONFIG_CACHE'][$config_key_uri])){
+		return $GLOBALS['_PLITE_CONFIG_CACHE'][$config_key_uri];
 	}
 	$path = explode('/', $config_key_uri);
 	$file = array_shift($path);
@@ -32,6 +36,63 @@ function get_config($config_key_uri, $ignore_on_file_no_exists = false, $nocache
 	if(!isset($config_file)){
 		throw new Exception("Config content empty in file:".$config_file);
 	}
-	$cache[$config_key_uri] = array_get($config, join('/', $path), null, '/');
-	return $cache[$config_key_uri];
+	$GLOBALS['_PLITE_CONFIG_CACHE'][$config_key_uri] = array_get($config, join('/', $path), null, '/');
+	return $GLOBALS['_PLITE_CONFIG_CACHE'][$config_key_uri];
+}
+
+/**
+ * Set configuration value to configuration file
+ * @param string $config_key_uri configuration name/path (e.g., "hello/world/yes")
+ * @param mixed $data value to set
+ * @return bool true on success
+ * @throws \Exception
+ */
+function set_config($config_key_uri, $data){
+	$path = explode('/', $config_key_uri);
+	$file = array_shift($path);
+
+	$config_file = PLITE_CONFIG_PATH."/$file.inc.php";
+	
+	// Load existing config or create empty array
+	if(is_file($config_file)){
+		$config = include $config_file;
+		if(!is_array($config)){
+			$config = [];
+		}
+	}else{
+		// Create config directory if not exists
+		$config_dir = dirname($config_file);
+		if(!is_dir($config_dir)){
+			if(!mkdir($config_dir, 0755, true)){
+				throw new Exception("Failed to create config directory: {$config_dir}");
+			}
+		}
+		$config = [];
+	}
+	
+	// Set value using path
+	if(empty($path)){
+		// If no path, replace entire config
+		$config = $data;
+	}else{
+		// Set nested value using array_push_by_path
+		array_push_by_path($config, join('/', $path), $data, '/');
+	}
+	
+	// Write config back to file
+	$export = var_export($config, true);
+	$content = "<?php\nreturn {$export};\n";
+	
+	if(file_put_contents($config_file, $content) === false){
+		throw new Exception("Failed to write config file: {$config_file}");
+	}
+	
+	// Clear all cache entries for this config file
+	foreach(array_keys($GLOBALS['_PLITE_CONFIG_CACHE']) as $cache_key){
+		if(strpos($cache_key, $file.'/') === 0 || $cache_key === $file){
+			unset($GLOBALS['_PLITE_CONFIG_CACHE'][$cache_key]);
+		}
+	}
+	
+	return true;
 }
